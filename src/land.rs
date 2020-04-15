ca273aee58813fff580b894a490211b7c0d5b90a
Jeremy M <muridaerattus@gmail.com>
2020-04-15 03:38:38 +0000

    land gen, for real this time

--- src/land.rs
+++ src/land.rs
@@ -1,1 +1,24 @@
+use super::dice;
 
+pub fn section_gen(sect: i8, aspect: i8)->[[char; 11]; 11]{
+    let mut section: [[char; 11]; 11] = [['0'; 11]; 11]; 
+    let mut r = 0;
+    for i in 0..11{
+        for j in 0..11{
+            r = dice::roll(2,4);
+            if(r == 3 || r == 5 || r == 7){
+                section[i][j] = 'E';
+            }else if(r == 2){
+                section[i][j] = 'D';
+            }else if(r == 8){
+                section[i][j] = 'V';
+            }else if(r == 4){
+                section[i][j] = 'C';
+            }else if(r == 6){
+                section[i][j] = 'R';
+            }
+        }
+    }
+    section[5][5] = 'H';
+    return section;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+mod land;
 mod dice;
+
+use std::fs::File;
+use std::io::Write;
+use std::env::current_dir;
 
 fn main() {
     println!("\0\0\0\0\0TABLESTUCK\nPRESS ENTER TO BEGIN");
@@ -7,4 +12,19 @@ fn main() {
     println!("{}",dice::roll(1,6)+3);
     println!("{}",dice::roll(1,6)+3);
     println!("{}",dice::roll(1,6)+3);
+    let mut landexample = land::section_gen(1,1);
+    println!("{}", landexample[6][6]);
+    println!("{}", landexample[1][3]);
+    let mut path = current_dir().unwrap().to_str().unwrap().to_string();
+    let mut landpath = path.clone() + "/../land.txt";
+    println!("Saving to {}", landpath);
+    let mut landfile = File::create(landpath).expect("Unable to create land file");
+    let mut b = [0; 2];
+    for i in 0..11{
+        for j in 0..11{
+            landfile.write_all(landexample[i][j].encode_utf8(&mut b).as_bytes()).expect("Can't save land");
+        }
+        landfile.write_all('\n'.encode_utf8(&mut b).as_bytes()).expect("Can't write new line");
+    }
+
 }
